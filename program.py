695a88d843ed3f95df5a61929212d88610b5e892
jawid-mirzad <47657846+jawid-mirzad@users.noreply.github.com>
2019-09-17 10:33:42 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,15 +1,15 @@
 # start
 
 # vakna upp
-vaken = "n"
+vaken = "n"                                           Det är ett värde.
 print("Du sover djupt  som björnen i ide...")
-while vaken == "n":
-    vaken = input("Vaknar du?[y/n] ").lower()
+while vaken == "n":                                   Det är ett värde om man svarar n det försättar fråga om vaken.
+    vaken = input("Vaknar du?[y/n] ").lower()         Man svarar y eller n.
 
 # duscha
-print("Du masar dig upp och släpar dig in i duschen.")
+print("Du masar dig upp och släpar dig in i duschen.") skriver ut filen.
 print("Någon har lämnat en brödrost i din dusch")
-duscha = input("Flyttar du på brödrosten? [y/n] ").lower()
+duscha = input("Flyttar du på brödrosten? [y/n] ").lower() 
 if duscha == "n":
     exit("Du elchockas och ditt äventyr är slut")
 elif duscha == "y":
@@ -19,7 +19,7 @@ else:
 
 # årstid
 season = False
-while season == False:
+while season == False:      De andra rader som är under while tillhör till vhile
     season = input("Vilken årstid är det? [vår, vinter, sommar, höst]").lower()
     if season == "vår" or season == "vinter" or season == "höst":
         print("Det är kallt och slask, fy tusan!")
